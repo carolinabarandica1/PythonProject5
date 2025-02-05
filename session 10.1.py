2e43cf4d7c787cc5addcd7d30cb557117b97852b
carolinabarandica <carolinabarandica1@gmail.com>
2025-02-05 18:04:53 +0100

    1st commit of the day

--- session 10.1.py
+++ session 10.1.py
@@ -1,3 +1,6 @@
+from types import new_class
+
+
 def greet():
     print("hello!")
 
@@ -6,3 +9,13 @@ simple function that just prints hello
 :return: None
 """
 print("hello!")
+
+def greet2(name):
+    """
+    simple function that greets a person
+    :param name: the name of a person
+    :return: None 
+    """"
+
+print(f"hello,{name}")
+
