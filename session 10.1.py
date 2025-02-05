b5e9a4a288970edc7e5fc6aa6203c495e46d559f
carolinabarandica <carolinabarandica1@gmail.com>
2025-02-05 18:27:54 +0100

    new commit

--- session 10.1.py
+++ session 10.1.py
@@ -41,3 +41,17 @@ print(special_op(2,10))
 result = special_op(8,9)
 print(f'the special op for 8 and 9 is: {result}')
 print(special_op(8,9))
+
+print(special_op())
+print(special_op(9))
+
+def bond_greet(name):
+    print(f'the name is: {name}')
+
+def bondise_name (first_name, last_name):
+    return f'{last_name}, {first_name}{last_name}'
+
+print(bondise_name('john', 'doe'))
+bond_greet(bondise_name(first_name='john', last_name='doe'))
+bond_greet(bondise_name())
+
