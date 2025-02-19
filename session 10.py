6d9201983ab06c1dbb93dda44aa09818c7f82b2b
carolinabarandica <carolinabarandica1@gmail.com>
2025-02-19 15:22:21 +0100

    new commit

--- session 10.py
+++ session 10.py
@@ -1,4 +1,10 @@
 import random
-sum = 0
-for i in range(18):
-    sum+= random.randint(1, 100)
+random_numbers = []
+for i in range (18):
+    num = random.randint(1,100)
+    random_numbers.append(num)
+
+filtered_numbers = [num * 2 for num in random_numbers if num %2 ==0]
+
+print(random_numbers)
+print("filtered_numbers and multiplied by 2", filtered_numbers)
